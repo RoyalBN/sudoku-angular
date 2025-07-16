21f5d9818c478921183cf96c2ae0b72c0f42f963
RoyalBN <sebastien.samson22@gmail.com>
2025-07-16 16:28:48 +0200

    :heavy_plus_sign: [CONFIG] Added PrimeNG library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
